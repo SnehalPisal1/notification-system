ab42781f4973e33a553febd18c9649ade4a0187b
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-11-26 18:37:22 +0100

    add UserRepository with JPA implementation

---
+++ src/main/kotlin/de/notification/api/notification/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package de.notification.api.notification.repository
+
+import de.notification.api.notification.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface UserRepository : JpaRepository<User, UUID>
+
