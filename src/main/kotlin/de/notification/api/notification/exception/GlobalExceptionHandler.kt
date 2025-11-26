577291d25a01860d96b99f77a670ed32b4850fca
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-11-26 18:41:06 +0100

    handle exceptions

---
+++ src/main/kotlin/de/notification/api/notification/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,13 @@
+package de.notification.api.notification.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+
+}
+
