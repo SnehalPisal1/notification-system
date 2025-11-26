7ea7700c8acff58d2e44f431244765b1b46a22d1
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-11-26 18:32:50 +0100

    add user entity

---
+++ src/main/kotlin/de/notification/api/notification/model/User.kt
@@ -0,0 +1,24 @@
+package de.notification.api.notification.model
+
+import de.notification.api.notification.util.NotificationTypeSetConverter
+import jakarta.persistence.Column
+import jakarta.persistence.Convert
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+import org.jetbrains.annotations.NotNull
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @Column(columnDefinition = "uuid")
+    val id: UUID,
+    @field:NotNull
+    @Convert(converter = NotificationTypeSetConverter::class)
+    var notifications: MutableSet<String> = mutableSetOf(),
+) {
+    // Default constructor for Hibernate
+    constructor() : this(UUID.randomUUID(), mutableSetOf())
+}
