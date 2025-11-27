66d63498f968aca086a34935093606a4a1d3100d
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-11-27 09:57:11 +0100

    refactor code

--- src/main/kotlin/de/api/notification/repository/UserRepository.kt
+++ src/main/kotlin/de/api/notification/repository/UserRepository.kt
@@ -1,6 +1,6 @@
-package de.notification.api.notification.repository
+package de.api.notification.repository
 
-import de.notification.api.notification.model.User
+import de.api.notification.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
