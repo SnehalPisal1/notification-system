482dc2de30099e822aeec6487dc1c9e0f738637a
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-11-30 16:13:52 +0100

    add service for business logic

---
+++ src/main/kotlin/de/api/notification/service/NotificationService.kt
@@ -0,0 +1,22 @@
+package de.api.notification.service
+
+import de.api.notification.exception.RateLimitException
+import de.api.notification.exception.UserNotFoundException
+import de.api.notification.exception.UserNotSubscribedException
+import de.api.notification.exception.UserRegistrationException
+import de.api.notification.model.NotificationDto
+import de.api.notification.model.RegisterRequest
+import de.api.notification.model.User
+import de.api.notification.repository.UserRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class NotificationService{
+
+
+
+}
+
+
+
