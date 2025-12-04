99faeb0b762a54ef3cb97bb3d50e8b6081b596c1
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-04 09:14:10 +0100

    configure Rete Limiter

--- src/main/kotlin/de/api/notification/config/RateLimiterConfig.kt
+++ src/main/kotlin/de/api/notification/config/RateLimiterConfig.kt
@@ -3,5 +3,9 @@ package de.api.notification.config
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ReteLimiterConfig{
+class ReteLimiterConfig (
+    @Value("\${spring.data.redis.host}") private val host: String,
+    @Value("\${spring.data.redis.port}") private val port: Int,
+    @Value("\${spring.data.redis.password:}") private val password: String
+    )
 }
