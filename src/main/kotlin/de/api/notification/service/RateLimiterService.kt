882a6c36a85fea4247c4e8bada55dfc6240c27ce
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-04 09:17:09 +0100

    implement Rate Limiter

--- src/main/kotlin/de/api/notification/service/RateLimiterService.kt
+++ src/main/kotlin/de/api/notification/service/RateLimiterService.kt
@@ -10,4 +10,19 @@ class RateLimiterService(
     @Value("\${ratelimit.capacity}") private val capacity: Long,
     @Value("\${ratelimit.refill-duration-seconds}") private val refillSeconds: Long
     ) {
+
+    fun tryConsume(userId: String): Boolean {
+        val limiter: RRateLimiter = redissonClient.getRateLimiter("user:$userId")
+
+        if (!limiter.isExists) {
+            limiter.trySetRate(
+                RateType.OVERALL,
+                capacity,
+                refillSeconds.toLong(),
+                RateIntervalUnit.SECONDS
+            )
+        }
+
+        return limiter.tryAcquire(1, 0, TimeUnit.SECONDS)
+    }
 }
