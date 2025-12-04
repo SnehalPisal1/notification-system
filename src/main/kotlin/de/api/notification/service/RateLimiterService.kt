f82e95ed77d54c9e5bbb5790df5f45eaee279e71
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-04 09:16:38 +0100

    implement Rate Limiter

--- src/main/kotlin/de/api/notification/service/RateLimiterService.kt
+++ src/main/kotlin/de/api/notification/service/RateLimiterService.kt
@@ -4,5 +4,10 @@ package de.api.notification.service
 import org.springframework.stereotype.Service
 
 @Service
-class RateLimiterService{
+class RateLimiterService(
+    private val redissonClient: RedissonClient,
+
+    @Value("\${ratelimit.capacity}") private val capacity: Long,
+    @Value("\${ratelimit.refill-duration-seconds}") private val refillSeconds: Long
+    ) {
 }
