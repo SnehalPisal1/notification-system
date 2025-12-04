4a067d9a1873b8e6a71c0d3dd6b71ee5a041bd9f
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-04 09:14:46 +0100

    configure Rete Limiter

--- src/main/kotlin/de/api/notification/config/RateLimiterConfig.kt
+++ src/main/kotlin/de/api/notification/config/RateLimiterConfig.kt
@@ -1,5 +1,10 @@
 package de.api.notification.config
 
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -7,5 +12,13 @@ class ReteLimiterConfig (
     @Value("\${spring.data.redis.host}") private val host: String,
     @Value("\${spring.data.redis.port}") private val port: Int,
     @Value("\${spring.data.redis.password:}") private val password: String
-    )
+    ) {
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        val server = config.useSingleServer()
+        server.address = "redis://$host:$port"
+        if (password.isNotBlank()) server.password = password
+        return Redisson.create(config)
+    }
 }
