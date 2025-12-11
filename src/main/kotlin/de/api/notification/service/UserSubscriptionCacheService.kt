076d88dc30641b28323f7aca5e43578564231cae
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-11 17:11:57 +0100

    implement cacheable get user subscriptions

---
+++ src/main/kotlin/de/api/notification/service/UserSubscriptionCacheService.kt
@@ -0,0 +1,14 @@
+package de.api.notification.service
+
+import de.api.notification.repository.UserRepository
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class UserSubscriptionCacheService(
+    private val userRepository: UserRepository
+) {
+
+}
