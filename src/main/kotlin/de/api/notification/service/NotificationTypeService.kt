6fce882b8cccb5b99b330fc9cda617a4a20474a9
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-17 14:49:02 +0100

    add logger

--- src/main/kotlin/de/api/notification/service/NotificationTypeService.kt
+++ src/main/kotlin/de/api/notification/service/NotificationTypeService.kt
@@ -7,8 +7,32 @@ import org.springframework.stereotype.Service
 
 @Service
 class NotificationTypeService(private val repo: NotificationTypeRepository) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
 
+
+    // Cached: type -> category
+    @Cacheable(cacheNames = ["typeCategory"], unless = "#result == null")
+    fun getCategoryForType(type: String): String? {
+        return repo.findById(type.trim())
+            .map { it.category }
+            .orElse(null)
+    }
+
+    // Cached: category -> types list
+    @Cacheable(cacheNames = ["typesForCategory"])
+    fun getAllTypesForCategory(category: String): List<String> {
+        return repo.findAllByCategory(category.trim())
+            .map { it.type }
+    }
+
+
+    fun isValidType(type: String): Boolean {
+        val isValid = repo.existsById(type)
+        logger.info("type = $type is= $isValid")
+
+        return isValid
+    }
 
 }
 
