12a521e26ed46d61b9c589907c96df59c971d4b0
SnehalPisal1 <snehal.pisal1005@gmail.com>
2025-12-22 14:51:19 +0100

    handle exception

--- src/main/kotlin/de/api/notification/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/de/api/notification/exception/GlobalExceptionHandler.kt
@@ -8,5 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalExceptionHandler {
 
+    @ExceptionHandler(UserNotFoundException::class)
+    fun handleUserNotFound(ex: UserNotFoundException) =
+        ResponseEntity.status(HttpStatus.NOT_FOUND).body(mapOf("error" to ex.message))
+
+
 }
 
